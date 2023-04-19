394aa5dbb78d63d88a992a6527bf87a0cf99cf38
Liam <liamjames1960@hotmail.com>
2023-04-19 13:50:11 +0200

    script correctly interacts with network (ganache)

--- tests/get_account_balance.js
+++ tests/get_account_balance.js
@@ -1,13 +1,15 @@
 const ethers = require("ethers");
 const ganache = require("ganache");
 
-const provider = new ethers.providers.Web3Provider(ganache.provider());
+const url = "http://localhost:8545"
+
+const provider = new ethers.providers.JsonRpcProvider(url);
 
 const ERC20_ABI = [
     "function addEntity(address _id, string memory _role) public"
 ]
 
-const address = "0xFFcf8FDEE72ac11b5c542428B35EEF5769C409f0"
+const address = "0x9060dA808374b7579565017b3c9baB8778606d7d"
 const contract = new ethers.Contract(address, ERC20_ABI, provider);
 
 const main = async() => {

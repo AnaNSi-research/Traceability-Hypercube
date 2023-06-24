2d979dc59ecc395eef8ce50f3fd844db02b3dd47
EmmanueleBollino <emmanuele.bollino@studio.unibo.it>
2023-06-24 10:04:49 +0200

    IPFS image and fixes

--- full_service/client-tracing/client.py
+++ full_service/client-tracing/client.py
@@ -1,4 +1,4 @@
-from solcx import compile_source, install_solc
+from solcx import compile_files, install_solc
 from eth_utils import address
 from web3 import Web3
 from eth_account import Account
@@ -9,38 +9,37 @@ import os
 
 
 class Client:
-    def __init__(self, blockchain_addr, chain_id, ipfs_addr, hypercube_addr, private_key = None):
+    def __init__(self, blockchain_addr="http://localhost:8545", chain_id="1337", ipfs_addr="/ip4/127.0.0.1/tcp/5001", hypercube_addr="localhost:8880", private_key = None):
         self.blockchain_addr = blockchain_addr
         self.chain_id = chain_id
         self.ipfs_addr = ipfs_addr
         self.hypercube_addr = hypercube_addr
         self.private_key = private_key
 
+        self.ipfs = ipfshttpclient.connect(ipfs_addr)
         self.w3 = Web3(Web3.HTTPProvider(blockchain_addr))
         
         if private_key is None:
             self.acct = self.w3.eth.accounts[0]
         else:
-            self.acct = Account.from_key(private_key)
+            self.acct = Account.from_key(private_key).address
         self.w3.eth.default_account = self.acct
+        print("Using account", self.acct)
 
         install_solc('0.8.19')
 
-    def compile_contract(self, sol_path):
-        with open(sol_path, "r") as file:
-            contract = file.read()
-        
-        compiled_sol = compile_source(contract, output_values=['abi', 'bin'])
+    def compile_contract(self, sol_path):        
+        compiled_sol = compile_files([sol_path], output_values=['abi', 'bin'])
         contract_id, contract_interface = compiled_sol.popitem()
 
-        bytecode = contract_interface['bin']
         abi = contract_interface['abi']
+        bytecode = contract_interface['bin']
 
         return abi, bytecode
 
     def deploy_factory(self, abi, bytecode, args={}):
         contract_bin = self.w3.eth.contract(abi=abi, bytecode=bytecode)
-        tx_hash = contract_bin.constructor(**args).transact({'from': self.acct.address})
+        tx_hash = contract_bin.constructor(**args).transact({'from': self.acct})
         tx_receipt = self.w3.eth.wait_for_transaction_receipt(tx_hash)
 
         self.contract = self.w3.eth.contract(
@@ -53,15 +52,18 @@ class Client:
 
         return self.contract
     
-    def create_car(self, brand, colour, img_path): #TODO test
-        ipfs_img_addr = "" #TODO deploy image on ipfs
-        create_car_tx = self.contract.functions.createCar(brand, colour, ipfs_img_addr).transact({"from": self.acct.address})
+    def create_car(self, brand, colour, img_path=None): #TODO test
+        if img_path is not None:
+            ipfs_img_addr = self.ipfs.add(img_path)['Hash']
+        else:
+            ipfs_img_addr = ""
+        print(ipfs_img_addr)
+
+        create_car_tx = self.contract.functions.createCar(brand, colour, ipfs_img_addr).transact({"from": self.acct})
         create_car_tx_receipt = self.web3.eth.wait_for_transaction_receipt(create_car_tx)
         receipt_car = self.web3.eth.get_transaction_receipt(create_car_tx)
+        print(receipt_car)
         #TODO add deployed car on hypercube
-    
-    #TODO create_car (through the factory contract)
-        # TODO add created car to hypercube
 
     #TODO search cars on the hypercube by keyword(s)
     
